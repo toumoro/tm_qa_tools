4e072adc4533a6ec0a585b0a9cf0f3071e88e91a
tm-hdaoud <haythem.daoud@toumoro.com>
2025-09-25 15:50:47 +0000

    feature/Add github ci & rewrite runTests.sh

--- services/configure.sh
+++ services/configure.sh
@@ -6,16 +6,22 @@ DEST_DIR="."
 
 # Copy Build Folder
 mkdir -p "${DEST_DIR}/build"
-cp -r "${QA_TOOLS_DIR}/build" "${DEST_DIR}/build"
-echo "Build folder copied to ${DEST_DIR}/build/"
+if [ ! -d "${DEST_DIR}/.githooks" ]; then
+    cp -r "${QA_TOOLS_DIR}/Build" "${DEST_DIR}/build"
+    echo "Build folder copied to ${DEST_DIR}/build/"
+fi
 
 # Copy .editorconfig File
-cp "${QA_TOOLS_DIR}/.editorconfig" "${DEST_DIR}/.editorconfig"
-echo ".editorconfig copied to ${DEST_DIR}/.editorconfig"
+if [ ! -d "${DEST_DIR}/.githooks" ]; then
+    cp "${QA_TOOLS_DIR}/.editorconfig" "${DEST_DIR}/.editorconfig"
+    echo ".editorconfig copied to ${DEST_DIR}/.editorconfig"
+fi
 
 # Copy tm_qa_tools.yml.dist
 mkdir -p "${DEST_DIR}/.github/workflows"
-cp ${QA_TOOLS_DIR}/.github/workflows/tm_qa_tools.yml.dist "${DEST_DIR}/.github/workflows/tm_qa_tools.yml"
-echo "Pipeline configuration copied to ${DEST_DIR}/.github/workflows/"
+if [ ! -d "${DEST_DIR}/.githooks" ]; then
+    cp ${QA_TOOLS_DIR}/.github/workflows/tm_qa_tools.yml.dist "${DEST_DIR}/.github/workflows/tm_qa_tools.yml"
+    echo "Pipeline configuration copied to ${DEST_DIR}/.github/workflows/"
+fi
 
 echo "All files checked and copied if missing."
